68f0bfaeb5e24f179e1980690a78461f6e7fd877
dogisgreat <git@bigly.dog>
2026-04-06 20:52:02 -0400

    -

---
+++ opt/claude-code/markdown-syndrome.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if ! [[ -v __CLAUDE_SESSION_ID ]]; then
+  set -x
+  exit 2
+fi
+
+# shellcheck disable=2154
+tmux new-window -a -c "$PWD" -- "$EDITOR" -- "./.markdown/$__CLAUDE_SESSION_ID.md"
