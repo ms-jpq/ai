90bd5973118466019cddbeed01040a817be0a6b4
dogisgreat <git@bigly.dog>
2026-04-06 21:44:31 -0400

    -

--- opt/claude-code/hooks/notification.sh
+++ opt/claude-code/hooks/notification.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 JSON="$(tee)"
-TITLE="$(jq --raw-output '.title' <<< "$JSON")"
+TITLE="$(jq --raw-output '.title // "Claude Code"' <<< "$JSON")"
 MESSAGE="$(jq --raw-output '.message' <<< "$JSON")"
 
 exec -- ~/.local/libexec/notify.kitty.sh /tmp/kitty.*.sock "$TITLE" "$MESSAGE"
