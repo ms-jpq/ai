a02d0d42973f573b03a92a54c5e4e0c64587dc92
dogisgreat <git@bigly.dog>
2026-04-08 11:54:15 -0400

    -

---
+++ opt/claude-code/kill-resurrection.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if INDEX="$("${0%/*}/hooks/session-file.sh" "$PWD")"; then
+  rm -fr -- "$INDEX"
+fi
