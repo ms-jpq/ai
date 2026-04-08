98ff3ecd80753f4af2d919914d74e5ff131e3e59
dogisgreat <hao@stainlessapi.com>
2026-04-08 15:00:05 -0400

    -

--- opt/chatty/mcp/pick.sh
+++ opt/chatty/mcp/pick.sh
@@ -6,7 +6,7 @@ JSON="${0%/*}/../../claude-code/mcp.json"
 SERVERS="$(jq --exit-status --raw-input --null-input --compact-output '[inputs]')"
 
 read -r -d '' -- JQ <<- 'JQ' || true
-[to_entries[] | select(.key | IN($servers[]))] | from_entries
+[to_entries[] | .value.type = "url" | select(.key | IN($servers[]))] | from_entries
 JQ
 
 jq --argjson servers "$SERVERS" "$JQ" < "$JSON" | envsubst
