3668e2965b54b449195e5fc48546608f35146f77
dogisgreat <git@bigly.dog>
2026-04-10 11:16:29 -0400

    -

--- opt/claude-code/hooks/shellcheck-hook.sh
+++ opt/claude-code/hooks/shellcheck-hook.sh
@@ -5,13 +5,6 @@ set -o pipefail
 JSON="$(tee)"
 FILE_PATH="$(jq -e --raw-output '.tool_input.file_path' <<< "$JSON")"
 
-case "$FILE_PATH" in
-*.sh) ;;
-*)
-  exit 0
-  ;;
-esac
-
 {
   shfmt --simplify --binary-next-line --space-redirects --indent=2 --write -- "$FILE_PATH" || exit 2
   shellcheck -- "$FILE_PATH" || exit 2
