4b62b99a44b65b1430d3f3d326b26f8ed3da998b
dogisgreat <git@bigly.dog>
2026-04-09 22:59:44 -0400

    -

--- opt/claude-code/hooks/save-diff.sh
+++ opt/claude-code/hooks/save-diff.sh
@@ -4,9 +4,8 @@ set -o pipefail
 
 JSON="$(tee)"
 SESSION_ID="$(jq --raw-output '.session_id' <<< "$JSON")"
+TOOL_NAME="$(jq --raw-output '.tool_name' <<< "$JSON")"
 FILE_PATH="$(jq --raw-output '.tool_input.file_path' <<< "$JSON")"
-OLD_STRING="$(jq --raw-output '.tool_input.old_string' <<< "$JSON")"
-NEW_STRING="$(jq --raw-output '.tool_input.new_string' <<< "$JSON")"
 
 BASE="${0%/*}"
 DIFFS="$(realpath -- "$BASE/../../../var/sessions")"
@@ -19,6 +18,21 @@ SUFFIX=""
 if [[ $EXT != "$BASENAME" ]]; then
   SUFFIX=".${EXT}"
 fi
+
+case "$TOOL_NAME" in
+Edit)
+  OLD_STRING="$(jq --raw-output '.tool_input.old_string' <<< "$JSON")"
+  NEW_STRING="$(jq --raw-output '.tool_input.new_string' <<< "$JSON")"
+  ;;
+Write)
+  NEW_STRING="$(jq --raw-output '.tool_input.content' <<< "$JSON")"
+  OLD_STRING=""
+  ;;
+*)
+  set -x
+  exit 2
+  ;;
+esac
 
 ENTRY_DIR="${DIFFS}/${SESSION_ID}/${TIMESTAMP}_${BASENAME}"
 mkdir -p -- "$ENTRY_DIR"
