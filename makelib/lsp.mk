e760e158b37edac5848867ffbbdd0cf2f9a94339
dogisgreat <git@bigly.dog>
2026-04-11 05:33:57 -0400

    -

--- makelib/lsp.mk
+++ makelib/lsp.mk
@@ -1,5 +1,5 @@
 .PHONY: lsp
 
 lsp: ./opt/claude-code/local-plugins/omnibus/.lsp.json
-./opt/claude-code/local-plugins/omnibus/.lsp.json: ~/.config/nvim/lsp.lua
+./opt/claude-code/local-plugins/omnibus/.lsp.json: ~/.config/nvim/lsp.lua ~/.config/nvim/ftdetect/mappings.json
 	'$<' > '$@'
