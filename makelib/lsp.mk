eb01086dfb55f5aca9ddeef86f5acf2bd03545af
dogisgreat <git@bigly.dog>
2026-04-11 05:09:17 -0400

    -

--- makelib/lsp.mk
+++ makelib/lsp.mk
@@ -5,4 +5,4 @@ LSP_TARGET := $(CC_DIR)/local-plugins/omnibus/.lsp.json
 
 lsp: $(LSP_TARGET)
 $(LSP_TARGET): $(CC_DIR)/libexec/lsp.lua $(CC_DIR)/libexec/lsp.jq $(CC_DIR)/libexec/filetypes.json
-	'$<' '$(CC_DIR)/libexec/lsp.jq' --slurpfile '$(CC_DIR)/libexec/filetypes.json' > '$@'
+	'$<' | '$(CC_DIR)/libexec/lsp.jq' --slurpfile map '$(CC_DIR)/libexec/filetypes.json' > '$@'
