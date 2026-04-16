8eff20f5b3669c6d2815c1e05a5f6679427fc44c
dogisgreat <git@bigly.dog>
2026-04-16 11:34:38 -0400

    -

---
+++ opt/claude-code/hooks/langfuse.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+JSON="$(tee)"
+"${0%/*}/../libexec/log-hooks.sh" "$0" <<< "$JSON"
