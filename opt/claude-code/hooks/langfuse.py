bd0d824617d1c001c7fa967961eb5f785f3f8535
dogisgreat <git@bigly.dog>
2026-04-16 11:22:13 -0400

    -

---
+++ opt/claude-code/hooks/langfuse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S -- PYTHONSAFEPATH= python3
+
+from contextlib import nullcontext
+from logging import INFO, basicConfig, captureWarnings
+
+with nullcontext():
+    captureWarnings(True)
+    basicConfig(format="%(message)s", level=INFO)
+
