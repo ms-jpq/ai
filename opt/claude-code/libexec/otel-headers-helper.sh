6ddc2ba546048a072ae3088b178aa729e9b48147
dogisgreat <git@bigly.dog>
2026-04-16 09:27:36 -0400

    -

---
+++ opt/claude-code/libexec/otel-headers-helper.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+# shellcheck disable=SC2154
+TOKEN="$(base64 -w0 <<< "$LANGFUSE_PUBLIC_KEY:$LANGFUSE_SECRET_KEY")"
+
+tee <<- JSON
+{"Authorization": "Bearer $TOKEN"}
+JSON
