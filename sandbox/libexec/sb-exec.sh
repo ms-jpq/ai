f2c80f4b80f6b7ac7951fbae33adbf9157b83d49
dogisgreat <git@bigly.dog>
2026-04-22 10:27:09 -0400

    -

---
+++ sandbox/libexec/sb-exec.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+OPTS=''
+LONG_OPTS=''
+GO="$(getopt --options="$OPTS" --longoptions="$LONG_OPTS" --name="$0" -- "$@")"
+eval -- set -- "$GO"
+
+while true; do
+  case "$1" in
+  -u | --user)
+    shift -- 2
+    ;;
+  -p | --pass)
+    shift -- 2
+    ;;
+  -h | --host)
+    shift -- 2
+    ;;
+  --video)
+    shift -- 1
+    ;;
+  --)
+    shift -- 1
+    break
+    ;;
+  *)
+    set -x
+    exit 2
+    ;;
+  esac
+done
+
+ROOT="$(realpath -- "${0%/*}/..")"
+
+# shellcheck disable=SC2154
+ARGV=(
+  sandbox-exec
+  -D PROFILES="$ROOT/darwin"
+  -D TMPDIR="$TMPDIR"
+  -D HOME="$HOME"
+  -D CWD="$PWD"
+  -D SSH_AUTH_SOCK="$SSH_AUTH_SOCK"
+)
+
+exec -- "${ARGV[@]}" "$@"
