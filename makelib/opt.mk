646a9e59561e390b11615bf89218b0f84b184f74
dogisgreat <git@bigly.dog>
2026-04-29 23:54:08 -0400

    -

--- makelib/opt.mk
+++ makelib/opt.mk
@@ -3,7 +3,7 @@
 CC := ./opt/claude-code
 
 lsp: $(CC)/local-plugins/omnibus/.lsp.json
-$(CC)/local-plugins/omnibus/.lsp.json: ~/.config/nvim/lsp.lua
+$(CC)/local-plugins/omnibus/.lsp.json: ~/.config/nvim/cc.lua
 	'$<' > '$@'
 
 cc: lsp
