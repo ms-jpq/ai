6d4336ffb6217b6d0948b2c56fb8d76a0e28ab70
dogisgreat <git@bigly.dog>
2026-06-14 05:09:07 -0700

    woah

--- opt/claude-code/worktree/libexec/tmux.sh
+++ opt/claude-code/worktree/libexec/tmux.sh
@@ -19,10 +19,28 @@ a | attach)
   SESSION="worktree/$NAME"
   "$SELF/tree.sh" init
   WORKTREE="$("$SELF/tree.sh" add "$NAME")"
+  printf -v QUOTED -- '%q' "$SESSION"
 
   TMP="$(mktemp).sh"
   {
+    ENV=TMUX_NO_SAVE
+
+    printf -- '%q ' tmux set-environment -g -h -- "$ENV" 1
+    printf -- '\n'
+
     printf -- '%q ' tmux new-window -c "$WORKTREE"
+    printf -- '\n'
+    printf -- '%q ' tmux set-buffer -- "claude --continue || claude --name $QUOTED "$'\n'
+    printf -- '\n'
+    printf -- '%q ' tmux paste-buffer -d -p
+    printf -- '\n'
+    printf -- '%q ' tmux select-pane -t '{marked}'
+    printf -- '\n'
+    printf -- '%q ' tmux select-pane -M
+    printf -- '\n'
+
+    # tmux select-window -t :-2
+    printf -- '%q ' tmux set-environment -g -h -u -- "$ENV"
   } > "$TMP"
   chmod +x -- "$TMP"
 
