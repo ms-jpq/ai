0f2a32d4d472d35512919cbc91a7fc0fa151458e
dogisgreat <git@bigly.dog>
2026-06-14 07:14:53 -0700

    fix

--- wagie/compile.sh
+++ wagie/compile.sh
@@ -3,7 +3,8 @@
 set -o pipefail
 
 CLAUDE_CONFIG_DIR="$1/.claude"
+SELF="${0%/*}"
 
-cp -af -- ./opt/claude-code/{agents,bin,hooks,libexec,rules,skills,AGENTS.md,keybindings.json} "$CLAUDE_CONFIG_DIR/"
+cp -af -- "$SELF/../opt/claude-code"/{agents,bin,hooks,libexec,rules,skills,AGENTS.md,keybindings.json} "$CLAUDE_CONFIG_DIR/"
 mv -- "$CLAUDE_CONFIG_DIR/AGENTS.md" "$CLAUDE_CONFIG_DIR/CLAUDE.md"
 rm -fr -- "$CLAUDE_CONFIG_DIR/skills/shitpost"
