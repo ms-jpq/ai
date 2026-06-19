94356f4ea89592664586dd041a2d7414a2d214b5
dogisgreat <git@bigly.dog>
2026-06-18 20:31:32 -0700

    adsf

--- opt/claude-code/hooks/notification.sh
+++ opt/claude-code/hooks/notification.sh
@@ -7,4 +7,35 @@ shopt -s dotglob nullglob extglob globstar
 JSON="$(tee)"
 # "${0%/*}/../libexec/log-hooks.sh" "$0" <<< "$JSON"
 
-exec -- "${0%/*}/../libexec/notify.sh" <<< "$JSON"
+EVENT="$(jq -e --raw-output '.hook_event_name' <<< "$JSON")"
+
+case "$EVENT" in
+Notification)
+  TYPE="$(jq -e --raw-output '.notification_type' <<< "$JSON")"
+  case "$TYPE" in
+  permission_prompt)
+    ;;
+  idle_prompt)
+    if jq --exit-status '.agent_type' <<< "$JSON" > /dev/null; then
+      exit
+    fi
+    ;;
+  *)
+    set -x
+    exit 2
+    ;;
+  esac
+  MESSAGE="$(jq -e --raw-output '.message' <<< "$JSON")"
+  ;;
+Stop | StopFailure)
+  MESSAGE="$(jq -e --raw-output '.last_assistant_message' <<< "$JSON")"
+  ;;
+*)
+  set -x
+  exit 2
+  ;;
+esac
+
+TITLE="Chatty ~ $(basename -- "${CLAUDE_PROJECT_DIR:="$PWD"}")"
+
+jq --null-input --arg title "$TITLE" --arg message "$MESSAGE" '{$title, $message}' | "${0%/*}/../libexec/notify.sh"
