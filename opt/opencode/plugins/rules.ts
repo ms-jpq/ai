0b13cf4c298a19b993a06fd7ba2885420b3b6413
dogisgreat <git@bigly.dog>
2026-07-04 01:16:45 -0700

    opencode

--- opt/opencode/plugins/rules.ts
+++ opt/opencode/plugins/rules.ts
@@ -1,14 +1,17 @@
 import { type Plugin } from "@opencode-ai/plugin"
 import { opendir } from "node:fs/promises"
+import { EOL } from "node:os"
 import { basename, join } from "node:path"
 import { env } from "node:process"
 
 const CONF_DIR = env["OPENCODE_CONFIG_DIR"] ?? ""
-const RULES = join(CONF_DIR, "..", "opt", "rules")
+const RULES = join(CONF_DIR, "..", "..", "opt", "opencode", "rules")
 
-export const Rules: Plugin = async () => {
+export const Rules: Plugin = async (_ctx) => {
   return {
     "experimental.chat.system.transform": async (_i, o) => {
+      o.system.push(`${EOL}**Rules**:${EOL}`)
+
       for await (const dirent of await opendir(RULES)) {
         const stem = basename(dirent.name, ".md")
         if (!dirent.isFile() || stem == dirent.name) {
@@ -17,7 +20,7 @@ export const Rules: Plugin = async () => {
 
         const path = join(dirent.parentPath, dirent.name)
 
-        o.system.push(`rule: ${stem} - @${path}`)
+        o.system.push(`rule: ${stem} → ${path}${EOL}`)
       }
     },
   }
